4cffd7e8b628f99d1549bbf5b457558826ed95c6
AlexAraldi <alexanderaraldi@gmail.com>
2025-06-11 15:30:45 -0300

    Implementado Projeto do Dominio

---
+++ ControleDeBar.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,14 @@
+﻿using ControleDeBar.Dominio.ModuloMesa;
+
+namespace ControleDeBar.Dominio.Compartilhado
+{
+    public abstract class EntidadeBase<T>
+    {
+        public Guid Id { get; set; }
+
+
+        public abstract void AtualizarRegistro(T atualizarRegistro);
+             
+
+    }
+}
---
+++ ControleDeBar.Dominio/Compartilhado/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ControleDeBar.Dominio.Compartilhado
+{
+    public interface IRepository<T> where T : EntidadeBase<T>
+    {
+        void CadastrarRegistro(T novoRegistro);
+        void EditarRegistro(Guid idRegistro, T registroEditado);
+        void ExcluirRegistro(Guid idRegistro);
+        List<T> SelecionarRegistros();
+        T? GetById(int id);
+        public T SelicionarRegistroPorid(Guid idRegistro);
+
+    }
+}
