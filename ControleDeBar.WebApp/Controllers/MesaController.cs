1dc07b5e826e314b7dc28676cda82a237caf2695
AlexAraldi <alexanderaraldi@gmail.com>
2025-06-13 18:23:48 -0300

    Implementado MesaViewModel, MesaController, MesaExtesions, IndexMesa, IndexHome, Layout.

---
+++ ControleDeBar.WebApp/Controllers/MesaController.cs
@@ -0,0 +1,29 @@
+﻿using ControleDeBar.Dominio.ModuloMesa;
+using ControleDeBar.Infraestrura.Arquivos.Compartilhado;
+using ControleDeBar.Infraestrutura.Arquivos.ModuloMesa;
+using ControleDeBar.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeBar.WebApp.Controllers
+{
+    [Route("Mesas")]
+    public class MesaController : Controller
+    {
+        private readonly ContextoDados contextoDados;
+        private readonly IRepositorioMesa repositorioMesa;
+
+        public MesaController()
+        {
+            contextoDados = new ContextoDados(true);
+            repositorioMesa = new RepositorioMesaEmArquivo(contextoDados);
+        }
+
+        [HttpGet]
+       public IActionResult Index()
+        {
+            var registro = repositorioMesa.SelecionarRegistros();
+            var visualizarVm = new VisualizarMesasViewModel(registro);
+            return View(visualizarVm);
+        }
+    }
+}
